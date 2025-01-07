efbb2bb9cd10c4205ccb146f8d55531e7eb6497a
Unaizahh <unaizah.qutub@student.tdsb.on.ca>
2025-01-07 13:36:26 -0500

    Update Main.java
    started menu using switch case

--- Main.java
+++ Main.java
@@ -5,6 +5,20 @@ public class Main {
     public static void main(String[] args) {
 
         System.out.println("Start");
+
+        Boolean menu = true;
+        Scanner c = new Scanner(System.in);  // Create a Scanner object
+        System.out.println("menu ramble... press one to start");
+        String userName = myObj.nextLine();
+        
+        while (menu == true) {
+            switch("c") {  
+                case "1":
+                    System.out.println("Start game:);
+                    menu = false;
+                    break;
+            }
+        }
       
     }
 }
