3d092d3e03a188a7241ccd1b7366f6de2af474d5
Unaizahh <unaizah.qutub@student.tdsb.on.ca>
2025-01-08 14:40:45 -0500

    Update Main.java

--- Main.java
+++ Main.java
@@ -1,41 +1,44 @@
 import java.util.*;
 
 public class Main {
-static Scanner options = new Scanner(System.in); //player options for the menu
-    
-static void menu(){
-    
-    System.out.println("menu ramble... press one to start");
-    String menu = options.nextLine();
-        
-        switch(menu) {  
-            case "p": //starting the game
+    static Scanner options = new Scanner(System.in); //player options for the menu
+
+    static void menu(){
+
+        // variables
+        Boolean start = true; // control menu
+
+        System.out.println("menu ramble... press s to start...press i for intructions");
+        String menu = options.nextLine();
+
+        switch(menu) {
+            case "s": //starting the game
                 System.out.println("Start game:");
-                //menu = false;
+                start = false;
                 break;
 
             case "i":
                 instructions();
                 menu();
                 break;
-                
-            }
+
         }
+    }
 
     static void instructions(){
         System.out.println("these are the very cool instructions");
         System.out.println("if you want to do this then blah blah blah...");
-        
+
     }
-        
+
     public static void main(String[] args) {
 
         menu();
-        //Boolean menu = true;
+
         //Scanner c = new Scanner(System.in);  // Create a Scanner object
         String userName = options.nextLine();
-        
 
-      
+
+
     }
 }
