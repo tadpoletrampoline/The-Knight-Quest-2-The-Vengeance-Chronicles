2fad38fd3e41e2e64595a96a4ca814d490ee96a2
tadpoletrampoline <tihami.islam@student.tdsb.on.ca>
2025-01-08 14:05:58 -0500

    Update Main.java

--- Main.java
+++ Main.java
@@ -11,13 +11,14 @@ static void menu(){
         switch(menu) {  
             case "P": //starting the game
                 System.out.println("Start game:);
-                menu = false;
+                //menu = false;
                 break;
             }
         }
         
     public static void main(String[] args) {
 
+        menu();
         /*Boolean menu = true;
         Scanner c = new Scanner(System.in);  // Create a Scanner object
         System.out.println("menu ramble... press one to start");*/
