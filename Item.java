c084ee732f12ba99d777999ac859e0c986f50805
tadpoletrampoline <tihami.islam@student.tdsb.on.ca>
2025-01-08 15:03:56 -0500

    Update Item.java

--- Item.java
+++ Item.java
@@ -2,15 +2,17 @@ public class Item {
 
     // instance variables (knight features)
 
-    
+    private String type; // there are type locks and type keys which can be used to perform their tasks
 
     // constructors (if given name or not)
 
 
   
     // getters 
-
     
+    public String getType() {
+        return this.type;
+    }
 
     // setters
     
