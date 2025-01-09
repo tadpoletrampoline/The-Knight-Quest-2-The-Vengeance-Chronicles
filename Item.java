eb456e6718b29ada9d6caa3c14c9f08a43256c47
tadpoletrampoline <tihami.islam@student.tdsb.on.ca>
2025-01-09 13:05:46 -0500

    Update Item.java

--- Item.java
+++ Item.java
@@ -1,8 +1,10 @@
 public class Item {
 
-    // instance variables (knight features)
+    // instance variables (item features)
 
     private String type; // there are type locks and type keys which can be used to perform their tasks
+    private String name; // item name
+    private int value; // i knid of forgot what i was going for with this in the uml but i feel like i'll remember eventually
 
     // constructors (if given name or not)
 
@@ -14,6 +16,14 @@ public class Item {
         return this.type;
     }
 
+    public String getName(){
+        return this.name;
+    }
+
+    public Int getValue(){
+        return this.value;
+    }
+    
     // setters
     
 }
