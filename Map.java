61d5af8e5f9ce5c6584a7eaf4390ad2621406dc9
Unaizahh <unaizah.qutub@student.tdsb.on.ca>
2025-01-15 14:11:21 -0500

    Update Map.java

--- Map.java
+++ Map.java
@@ -1,145 +1,78 @@
 import java.util.*;
 
-// class to create mapping gample including movement and inserting puzzles
+public class Main {
+
+    // variables
+    ArrayList<Item> items = new ArrayList<Item>(); // Create an ArrayList object of type Item
+
+    //setting up the game
+    int score = 0;
+    boolean hasItem = false;
+    int location = 1; // signifies location/level (we could use a while loop or something)
+
+    Scanner controls = new Scanner(System.in); //let's user input their actions
     
-    public class Map {
+    static Scanner options = new Scanner(System.in); //player options for the menu
+    
+    static void menu(){
         
-    //instance variables
+        Boolean start = true; // control menu
         
-        // delete private int Row,Col,Num1,Num2;
-        private int p1 = 0;
-        private int p2 = 0;
-        private String Move;
-        private boolean Running = true;
-        int Array[][];
-        private int size = 7; // map size hard coded (change maybe later) 
-        private int level; 
-        String key; // key name, not object (in item class)
-        
-        // constructor !!!
-        public Map(int level) {
-        
-        this.level = level;
-        this.Array = new int [this.size][this.size];
-        
-        // assigning key names per level
-        if (this.level == 1) {
-            key = "key";
-        } else if (this.level == 2) {
-            key = "axe";
-        } else if (level == 3) {
-            key = "magic wand";
-        }
-        
-        // create grid             
-        for(int i=0;i<size;i++) {
-            for(int x=0;x<size;x++) {
-                this.Array[i][x] = 0;
-                this.Array[p1][p2] = 1;
-                
-                // creates patterns of making values 2 as borders * will edit later to be a complex path
-                if( level == 1) {
-                    if (((x+i)^5)/2 == 0 && x<size-1 && i<size-1  && i<size-1 && x>0 && i>0) {
-                        this.Array[i][x] = 2;
-                        this.Array[this.p1][this.p2] = 1;
-                    }
-                }
-                
-                // adds the key in a random spot on the map
-                //this.Array[p1][p2] = (Math.random() * 7.00); //hard coded random range
+        System.out.println("menu ramble... press s to start...press i for intructions");
+        String menu = options.nextLine();
 
-            }
-        }
-        
-        // print grid for debugging purposed
-        for(int i=0;i<size;i++) {
-            for(int x=0;x<size;x++) {
-                System.out.print(this.Array[i][x] + "\t");
-            }
-            System.out.println();
-        }
-        
-        }
-        
-    public void move() {
-        
-        while(Running) {
-            
-            Scanner input = new Scanner(System.in);
-            this.Move = input.next(). substring(0,2);
-            
-            switch(Move) {
-            case "so":
-            case "So":
+        switch(menu) {
+            case "s": //starting the game
+                System.out.println("\nstarting game... ... ... ...\n");
+                start = false;
+                gameIntro();
                 
-            if (p1 < size-1 && Array[p1+1][p2] != 2) { //prevents out of bounds
-                Array[p1][p2] = 0;
-                Array[p1+=1][p2] = 1;
-                break;
-            } else {
-                System.out.println("Your path here is blocked, try another way.");
-                break;
-                }
                 
-            case "no":
-            case "No":
-                
-                if (p1 > 0 && Array[p1-1][p2] != 2) { //prevents out of bounds
-                    Array[p1][p2] = 0;
-                    Array[p1-=1][p2] = 1;
-                    break;
-                } else {
-                    System.out.println("Your path here is blocked, try another way.");
-                    break;
-                }
-            case "ea":
-            case "Ea":
-                
-            if (p2 < size-1 && Array[p1][p2+1] != 2) {   
-                Array[p1][p2] = 0;
-                Array[p1][p2+=1] = 1;
+                Map landscape = new Map(1); // initializing map object!!!
+                landscape.move(); 
                 break;
-            } else {
-                    System.out.println("Your path here is blocked, try another way.");
-                    break;
-                }
-                
-            case "we":
-            case "We":
-            
-            if (p2 > 0 && Array[p1][p2-1] != 2) { 
-                Array[p1][p2] = 0;
-                Array[p1][p2-=1] = 1;
-                break;
-            } else {
-                    System.out.println("Your path here is blocked, try another way.");
-                    break;
-                }
-            
-            case "le":
-            case "Le":
-                System.out.println("You have exited the game...");
-                Running = false;
-                input.close();
-                break;
-            default:
-                System.out.println("I don't understand that.");
-                break;
-        }
-        
-        for(int i=0;i<size;i++) {
-            for(int x=0;x<size;x++) {
-                System.out.print(Array[i][x] + "\t");
-        }
-            System.out.println(); // temporary printing of map for debugging
-                }
-            }
-            
-        }
 
-      // getters
+            case "i":
+                instructions();
+                menu();
+                break;
+                
+            case "0": //lets player exit game
+                System.out.println("bye bye");
+                System.exit(0);
 
-      // setters
-      
+            default: 
+                System.out.println("how dare you, please type in one of the proper options");
+                menu(); // recursion!!!
+                break;
+        }
     }
-    
+
+static void instructions(){
+        System.out.println("these are the very cool instructions");
+        System.out.println("if you want to do this then blah blah blah...");
+
+    }
+
+    //i'm going to add all this stuff to a class soon maybe
+    static void gameIntro(){
+        System.out.println("Enter your username (default is Lancelot): ");
+
+        String username = options.nextLine();
+        Knight person = new Knight(username);                  /// knight object created!!
+        // System.out.println(person.getName()); // testing if name works
+    }
+
+    static void levelOne(){
+        System.out.println("this is level one");
+    }
+
+    public static void main(String[] args) {
+
+        // lets player start the game
+        menu();
+
+
+
+    }
+}
