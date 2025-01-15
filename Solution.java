3a314a7fb77e79874b89335ed8cbfa3ea56f3870
Unaizahh <unaizah.qutub@student.tdsb.on.ca>
2025-01-15 13:54:37 -0500

    Create Solution.java
    added subclass of item class to specify solution items (or "keys") for levels

---
+++ Solution.java
@@ -0,0 +1,18 @@
+public class Solution extens Item {
+
+    // instance variables (item features)
+
+    private String type; // there are type locks and type keys which can be used to perform their tasks
+    private String name; // item name
+    private int value; // i knid of forgot what i was going for with this in the uml but i feel like i'll remember eventually
+
+    // constructor
+    
+ 
+  
+    // getters 
+    
+    
+    // setters
+    
+}
