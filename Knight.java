30bc2bcc26663f78460856211290ddd3b6990dd5
Unaizahh <unaizah.qutub@student.tdsb.on.ca>
2025-01-17 10:37:56 -0500

    Update Knight.java

--- Knight.java
+++ Knight.java
@@ -5,17 +5,19 @@ public class Knight {
     // instance variables (knight features)
     private String name;
     private int score;
+    private int health; 
 
     // constructors (if given name or not)
 
-    public Knight(String name) {
+    public Knight(String name, int health) {
         
         if (name.equals("")) {
             this.name = "Lancelot";
         } else {
             this.name = name;
         }
-        
+
+        this.health = health; // change health level of player depending on level to increase difficulty
         this.score = 0;
     }
   
