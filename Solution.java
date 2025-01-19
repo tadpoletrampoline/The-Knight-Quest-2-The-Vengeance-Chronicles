bef19c3ed5e3c6b34650f8a3aedd1c6f22566c5c
tadpoletrampoline <tihami.islam@student.tdsb.on.ca>
2025-01-18 22:04:14 -0500

    Update Solution.java

--- Solution.java
+++ Solution.java
@@ -1,19 +1,21 @@
 public class Solution extends Item {
 
-    // instance variables (item features)
+	// instance variables (item features)
 
-    /*private String type; // there are type locks and type keys which can be used to perform their tasks
-    private String name; // item name
-    private int value; // i knid of forgot what i was going for with this in the uml but i feel like i'll remember eventually
-    */
+	/*private String type; // there are type locks and type keys which can be used to perform their tasks
+	private String name; // item name
+	private int value; // i knid of forgot what i was going for with this in the uml but i feel like i'll remember eventually
+	*/
 
-    // constructor
-    
- 
-  
-    // getters 
-    
-    
-    // setters
-    
+	// constructor
+    public Solution(String name, String type) {
+        super(name, type);
+    }
+
+
+	// getters
+
+
+	// setters
+
 }
