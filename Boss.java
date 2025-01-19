c193a32fc58257bddb7fbfa7ec52e8d37888574d
tadpoletrampoline <tihami.islam@student.tdsb.on.ca>
2025-01-19 12:52:58 -0500

    Update Boss.java
    updated formatting in boss level

--- Boss.java
+++ Boss.java
@@ -1,7 +1,27 @@
+
 import java.util.*;
 import java.io.*;
 
 public class Boss {
+    //Coloured Text
+    static String RED = "\u001B[31m";
+    static String BLACK = "\u001B[30m";
+    static String YELLOW = "\u001B[33m";
+    static String MAGENTA = "\u001B[35m";
+    static String BLUE = "\u001B[34m";
+    static String RESET = "\u001B[0m";
+    static String BOLD = "\n\033[0;1m";
+    static String UNBOLD = "\033[0;0m";
+
+    //Highlighted Text
+    static String BLACK_BG = "\u001B[40m";
+    static String RED_BG = "\u001B[41m";
+    static String GREEN_BG = "\u001B[42m";
+    static String YELLOW_BG = "\u001B[43m";
+    static String BLUE_BG = "\u001B[44m";
+    static String PURPLE_BG = "\u001B[45m";
+    static String CYAN_BG = "\u001B[46m";
+    static String WHITE_BG = "\u001B[47m";
 
     private int guess; // intialize guess variable for number guessing
 
@@ -14,16 +34,16 @@ public class Boss {
     public Boss() {
 
         // Adding riddles and their answers for phase 1
-        riddles1.put("What goes up but never comes down?", "age");
-        riddles1.put("A time when they're green. A time when they're brown. But both of these times, cause me to frown. But just in between, for a very short while. They're perfect and yellow. And cause me to smile.", "banana");
-        riddles1.put("I fly, yet I have no wings. I cry, yet I have no eyes. Darkness follows me. Lower light I never see.", "cloud");
-        riddles1.put("What can you catch but not throw?", "cold");
+        riddles1.put("\u001B[31mWhat goes up but never comes down?\u001B[0m", "age");
+        riddles1.put("\u001B[31mA time when they're green. A time when they're brown. But both of these times, cause me to frown. But just in between, for a very short while. They're perfect and yellow. And cause me to smile.\u001B[0m", "banana");
+        riddles1.put("\u001B[31mI fly, yet I have no wings. I cry, yet I have no eyes. Darkness follows me. Lower light I never see.\u001B[0m", "cloud");
+        riddles1.put("\u001B[31mWhat can you catch but not throw?\u001B[0m", "cold");
 
         // Adding riddles and their answers for phase 2
-        riddles2.put("Give it food and it will live, give it water and it will die.", "fire");
-        riddles2.put("I have a tail and a head, but no body. What am I?", "coin");
-        riddles2.put("Long and think, red within, with a nail at the end.", "finger");
-        riddles2.put("What has keys but can't open locks?", "piano");
+        riddles2.put("\u001B[31mGive it food and it will live, give it water and it will die.\u001B[0m", "fire");
+        riddles2.put("\u001B[31mI have a tail and a head, but no body. What am I?\u001B[0m", "coin");
+        riddles2.put("\u001B[31mLong and think, red within, with a nail at the end.\u001B[0m", "finger");
+        riddles2.put("\u001B[31mWhat has keys but can't open locks?\u001B[0m", "piano");
 
         // Start the game
         game1();
@@ -32,9 +52,9 @@ public class Boss {
 
     public void game1() {
 
-        System.out.println("It is impressive that you've followed me this far... I shall put you under a spell so you don't attack me. If you wish to free yourself, all you have to do is player a few games with me.");
-        System.out.println("I shall share with you a riddle, you must give me the correct answer, you have 5 guesses");
-        System.out.println("(All answers are one word and singular, good luck!)");
+        System.out.println(RED + "It is impressive that you've followed me this far... I shall put you under a spell so you don't attack me. If you wish to free yourself, all you have to do is player a few games with me." + RESET);
+        System.out.println(RED + "I shall share with you a riddle, you must give me the correct answer, you have 5 guesses" + RESET);
+        System.out.println(RED + "\n(All answers are one word and singular, good luck!)\n\n" + RESET);
 
         int limit = 5; // limited guesses
         int guesses = 0; // keep track of guesses
@@ -54,20 +74,20 @@ public class Boss {
             if (guesses < limit) { // guess if below limit
 
                 Scanner input = new Scanner(System.in);
-                System.out.print("Enter your guess: ");
+                System.out.print(BLUE + "\nEnter your guess: ");
                 String guess = input.nextLine();
 
                 if (guess.toLowerCase().equals(answer)) {
-                    System.out.println("Wizard: Oh you actually guessed correctly... You shall now play another game.");
+                    System.out.println("\n" +RED + "Wizard: " + BOLD + YELLOW + "Oh you actually guessed correctly... You shall now play another game." + RESET);
                     this.guessing = false;
                     game2();
                 } else {
                     guesses++;
-                    System.out.printf("Wizard: Incorrect Mwahaha! You only have %d guesses left!\n", limit-guesses);
+                    System.out.printf("\n" + RED + "Wizard: " + BOLD + YELLOW + " Incorrect Mwahaha! You only have %d guesses left!\n" + RESET, limit-guesses);
                 }
             } else {
-                System.out.println("Wizard: Haha you lost! (You ran out guesses");
-                System.out.println("(Would you like to play again? Y or N");
+                System.out.println("\n"+ RED + "Wizard: " + BOLD + YELLOW+ " Haha you" + RED+" lost!" + RESET +" (You ran out guesses)\n");
+                System.out.println(BLUE + "(Would you like to play again?\n\n"+GREEN_BG+ "Y" + RESET+ " or " + RED_BG + "N" + RESET);
 
                 // replay stuff
                 Scanner input = new Scanner(System.in);
@@ -123,8 +143,8 @@ public class Boss {
         int target = numbers[random.nextInt(numbers.length)]; // sort array for binary search
 
         int index = binarySearch(numbers, target);
-        System.out.println("Wizard: I'm thinking a number from 1-100, guess where it is this a list of 35 numbers");
-        System.out.println("(Guess the number in the list. Hint: Use binary search methods to help guess the number faster.)");
+        System.out.println("\n" +RED+ "Wizard: " + YELLOW + " I'm thinking a number from 1-100, guess where it is this a list of 35 numbers" +RESET);
+        System.out.println(BLUE + "(Guess the number in the list. Hint: Use binary search methods to help guess the number faster.)\n" + RESET);
 
         while (this.guessing) {
 
@@ -132,7 +152,7 @@ public class Boss {
 
                 System.out.println(Arrays.toString(numbers)); // show list
 
-                System.out.print("Enter a number: ");
+                System.out.print(BLUE + "\nEnter a number: " + RESET);
                 Scanner scanner = new Scanner(System.in);
 
                 // getting guess
@@ -140,25 +160,24 @@ public class Boss {
                     guess = scanner.nextInt();
                 } catch (InputMismatchException e) {
                     scanner.next();
-                    System.out.println("Invalid input. Please enter a valid number.");
+                    System.out.println("\nInvalid input. Please enter a valid number.\n\n");
                     guess = scanner.nextInt();
                 }
 
                 if (guess == target) {
-                    System.out.println("Wizard: Oh you actually guessed correctly again... You shall now play a final game.");
+                    System.out.println(RED + "\n\nWizard: " + YELLOW + " Oh you actually guessed correctly again... You shall now play " + RESET + RED_BG + BLACK + "a final game."+ RESET + "\n\n");
                     game3();
                     this.guessing = false;
                 } else if (guess < target) {
                     guesses++;
-                    System.out.printf("Wizard: Higher! Only %d guesses left!\n", limit-guesses);
+                    System.out.printf(RED + "\nWizard: " + YELLOW + " Higher! Only %d guesses left!\n" + RESET, limit-guesses);
                 } else if (guess > target) {
                     guesses++;
-                    System.out.printf("Wizard: Lower! Only %d guesses left!\n", limit-guesses);
+                    System.out.printf(RED + "Wizard: " + YELLOW + BOLD + " Lower! Only %d guesses left!\n" + RESET, limit-guesses);
                 }
             } else {
-                System.out.println("Haha you lost! (You ran out guesses)");
-                System.out.println("Wizard: Haha you lost! You ran out guesses!");
-                System.out.println("(Would you like to play again? Y or N)");
+                System.out.println("\n"+ RED + "Wizard: " + BOLD + YELLOW+ " Haha you" + RED+" lost!" + RESET +" (You ran out guesses)\n");
+                System.out.println(BLUE + "(Would you like to play again?\n\n"+RESET+GREEN_BG+ "Y" + RESET+ " or " + RED_BG + "N" + RESET);
 
                 // replay stuff
                 Scanner input = new Scanner(System.in);
@@ -195,20 +214,20 @@ public class Boss {
             if (guesses < limit) { // guess if below limit
 
                 Scanner input = new Scanner(System.in);
-                System.out.print("Enter your guess: ");
+                System.out.print(BLUE + "\nEnter your guess: " + RESET);
                 String guess = input.nextLine();
 
                 if (guess.toLowerCase().equals(answer)) {
-                    System.out.println("Oh you actually beat me at my own game.. BUT STILL I CANNOT LET YOU LIVE!!! .");
+                    System.out.println("\n\n\nOh.... you actually beat me at my own game.. " + RED_BG+ BLACK + " BUT STILL I CANNOT LET YOU LIVE!!! ." + RESET);
                     this.guessing = false;
                     new MyFrame(3); // cutscene
                 } else {
                     guesses++;
-                    System.out.printf("Wizard: Incorrect Mwahaha! You only have %d guesses left!", limit-guesses);
+                    System.out.printf("\n"+ RED +"Wizard:" + YELLOW + BOLD + " Incorrect Mwahaha! You only have %d guesses left!\n" + RESET, limit-guesses);
                 }
             } else {
-                System.out.println("Wizard: Haha you lost! You ran out guesses!");
-                System.out.println("(Would you like to play again? Y or N)");
+                System.out.println("\n"+ RED + "Wizard: " + BOLD + YELLOW+ " Haha you" + RED+" lost!" + RESET +" (You ran out guesses)\n");
+                System.out.println(BLUE + "(Would you like to play again?\n\n"+GREEN_BG+ "Y" + RESET+ " or " + RED_BG + "N" + RESET);
 
                 // replay stuff
                 Scanner input = new Scanner(System.in);
@@ -227,4 +246,3 @@ public class Boss {
         }
     }
 }
-
